bbed315a582b4bd66d22aee55763008ce360c606
rodi38 <rodrigorochaed1@gmail.com>
2023-03-29 17:58:50 -0300

    CategoriaService completa

--- src/main/java/com/livrariasantanler/santanler/service/CategoriaService.java
+++ src/main/java/com/livrariasantanler/santanler/service/CategoriaService.java
@@ -1,11 +1,59 @@
 package com.livrariasantanler.santanler.service;
 
+import com.livrariasantanler.santanler.model.dto.CategoriaDTO;
+import com.livrariasantanler.santanler.model.entity.CategoriaEntity;
 import com.livrariasantanler.santanler.model.mapper.CategoriaMapper;
 import com.livrariasantanler.santanler.repository.CategoriaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
     private CategoriaRepository repository;
-    private CategoriaMapper categoriaMapper;
+    private CategoriaMapper mapper;
+
+    public CategoriaDTO pegarPorId(Long id) {
+        Optional<CategoriaEntity> categoriaEntityOptional = repository.findById(id);
+        if (categoriaEntityOptional.isPresent()) {
+            CategoriaEntity categoriaEntity = categoriaEntityOptional.get();
+            return mapper.update(categoriaEntity);
+        }
+        throw new EntityNotFoundException("Categoria não encontrada, revisa isso ai meu chapa!");
+    }
+
+    public CategoriaDTO criar(CategoriaDTO categoriaDTO) {
+        CategoriaEntity categoria = mapper.update(categoriaDTO);
+        categoria = repository.save(categoria);
+        return mapper.update(categoria);
+    }
+
+    public CategoriaDTO editar(CategoriaDTO categoriaDTO, Long id) {
+        if (repository.existsById(id)){
+            CategoriaEntity categoriaEntity = mapper.update(categoriaDTO);
+            categoriaEntity.setId(id);
+            categoriaEntity = repository.save(categoriaEntity);
+            return mapper.update(categoriaEntity);
+        }
+        throw new EntityNotFoundException("Categoria não encontrada, revisa isso ai meu chapa!");
+    }
+    public void deletar(Long id){
+        Optional<CategoriaEntity> categoriaEntityOptional = repository.findById(id);
+        if(categoriaEntityOptional.isPresent()){
+            CategoriaEntity categoriaEntity = categoriaEntityOptional.get();
+            repository.delete(categoriaEntity);
+            return;
+        }
+        throw new EntityNotFoundException("Categoria não encontrada, revisa isso ai meu chapa!");
+    }
+
+    public List<CategoriaDTO> listar() {
+        List<CategoriaEntity> listaEntities = repository.findAll();
+        return mapper.updateListDTO(listaEntities);
+    }
+
 }
